31b40665b70018f2656ff6fc84f879b47ab95f74
Henrick Neads <henrick.neads@students.hevs.ch>
2022-04-05 23:12:13 +0200

    add of naming animation

---
+++ js/nameAnimation.js
@@ -0,0 +1,11 @@
+
+const txtAnm = document.querySelector('h2')
+
+new Typewriter(txtAnm,{
+})
+.typeString('Moi c\'est Henrick Neads')
+.pauseFor(1500)
+.typeString('<br/>')
+.typeString('Junior Web Developper')
+.start()
+
